bbfa432ff4ba2ffc51fd4473e7b0a8675dc1ce18
Ashraful Bari Chowdhury <b.ashraful@gmail.com>
2020-10-10 11:14:06 -0700

    started to set up firebase

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 
 import Header from './components/header/header.component';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -23,10 +23,12 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      // this.setState({ currentUser: user });
 
-      console.log(user);
+      createUserProfileDocument(user);
+
+      // console.log(user);
     });
   }
 
