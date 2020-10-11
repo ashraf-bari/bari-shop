2196d6530f94039e2d7d1b1de4edb770d9ab121a
Ashraful Bari Chowdhury <b.ashraful@gmail.com>
2020-10-10 17:45:27 -0700

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -33,9 +33,10 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => {
-            console.log(this.state);
           });
+
+          console.log(this.state);
+
         });
 
       }
