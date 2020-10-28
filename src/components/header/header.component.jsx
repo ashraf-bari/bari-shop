69c8721cd9165cbb6d5f645aeca8e094a5695bdf
Ashraful Bari Chowdhury <b.ashraful@gmail.com>
2020-10-28 16:07:00 -0700

    added redux and implemented userReducer and userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -35,5 +36,8 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+});
 
-export default Header;
+export default connect(mapStateToProps)(Header);
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+const MenuItem = ({ title, imageUrl, linkUrl, size, history, match }) => (
     <div
         className={`menu-item ${size}`}
         onClick={() => history.push(`${match.url}${linkUrl}`)}
