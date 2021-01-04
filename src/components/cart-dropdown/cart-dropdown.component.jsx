4b1aaa0fd46a96ed2ae2cb1c227dbf039c670e69
Ashraful Bari Chowdhury <b.ashraful@gmail.com>
2021-01-04 05:29:32 -0800

    added checkout page, checkout item component and updated cart to use selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+
+import { toggoleDropdownHidden } from '../../redux/cart/cart.actions';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
-                cartItems.map((cartItem) => (<CartItem key={cartItem.id} item={cartItem} />))
+                cartItems.length ?
+                    (cartItems.map((cartItem) => (<CartItem key={cartItem.id} item={cartItem} />))) : (
+                        <span className='empty-message'>Your cart is empty</span>)
             }
         </div>
-        <CustomButton className='button'>G0 TO CHECKOUT</CustomButton>
+        <CustomButton className='button' onClick={() => {
+            history.push('/checkout');
+            dispatch(toggoleDropdownHidden());
+        }}>
+            G0 TO CHECKOUT
+        </CustomButton>
     </div>
 );
 
@@ -24,4 +34,4 @@ const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems,
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -18,3 +18,10 @@ export const selectCartItemsCount = createSelector(
         ((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity), 0
     )
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce(
+        (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity * cartItem.price, 0
+    )
+);
